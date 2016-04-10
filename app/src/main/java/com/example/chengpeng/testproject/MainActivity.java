a7daaee4df4629d66b47469029a401ead671313a
chengpeng0409 <pengchengkevin@hotmail.com>
2016-04-10 15:48:14 -0700

    1st change

--- app/src/main/java/com/example/chengpeng/testproject/MainActivity.java
+++ app/src/main/java/com/example/chengpeng/testproject/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            return false;
         }
 
         return super.onOptionsItemSelected(item);
